313854981ece96b655d93d1559c9b022099d860f
aadigwe <adigwe.a@husky.neu.edu>
2019-04-20 20:28:59 -0400

    Update extract features

--- extractfeatures.py
+++ extractfeatures.py
@@ -40,6 +40,14 @@ def get_labels(path=Data_Directory):
 	label_indices = np.arange(0, len(labels))
 	return labels, label_indices, np_utils.to_categorical(label_indices)
 
+def wav2mfcc(file_path, max_pad_len=120):
+    wave, sr = librosa.load(file_path, mono=True, sr=None)
+    wave = wave[::3]
+    mfcc = librosa.feature.mfcc(wave, sr=16000)
+    pad_width = max_pad_len - mfcc.shape[1]
+    mfcc = np.pad(mfcc, pad_width=((0, 0), (0, pad_width)), mode='constant')
+    return mfcc
+
 def save_features_to_array(path = Data_Directory):
 	labels, _, _ = get_labels(path)
 	print(labels)
@@ -49,7 +57,7 @@ def save_features_to_array(path = Data_Directory):
 		mfcc_vectors = []
 		sp_vectors = []
 
-		wavfiles = [path + label + '/' + wavfile for wavfile in os.listdir(path + '/' + label)]
+		wavfiles = [path + label + '/' + wavfile for wavfile in sorted(os.listdir(path + '/' + label))]
 		for wavfile in wavfiles:
 			print(wavfile)
 			x, fs = sf.read(wavfile)
@@ -64,15 +72,24 @@ def save_features_to_array(path = Data_Directory):
 			ap = pw.d4c(x, f0, t, fs)
 			ap_vectors.append(ap)
 
-			mfcc = librosa.feature.mfcc(x, sr=16000)
+			mfcc = wav2mfcc(wavfile, max_pad_len=120)
 			mfcc_vectors.append(mfcc)
-
-		np.save('fundfreq_' + label + '.npy', fundfreq_vectors)
-		np.save('sp_' + label + '.npy', sp_vectors)
-		np.save('ap_' + label + '.npy', ap_vectors)
+		#print(mfcc_vectors.shape)
 		np.save('mfcc_' + label + '.npy', mfcc_vectors)
+		np.save('fundfreq_' + label + '.npy', fundfreq_vectors)
+		#np.save('sp_' + label + '.npy', sp_vectors)
+		#np.save('ap_' + label + '.npy', ap_vectors)
+		#np.save('mfcc_' + label + '.npy', mfcc_vectors)
 
 save_features_to_array(Data_Directory)
+
+
+#C. Split Training and test states
+def get_train_test(split_ratio=0.6, random_state=42):
+	labels, indices, _ = get_labels(DATA_PATH)
+	X = np.load(labels[0] + '.npy')
+	y = np.zeros(X.shape[0])
+
 
 #B. LOOP to extract vector by sample of target fundamental frequency
 
--- read_features.py
+++ read_features.py
@@ -3,10 +3,10 @@ import numpy as np
 # Read Fundamental Frequency
 anger_f0 = np.load('fundfreq_anger.npy')
 neut_f0 = np.load('fundfreq_neutral.npy')
-
 print(anger_f0.shape)
 print(neut_f0.shape)
+print(anger_f0[1]) #To access each individual audiofile fundfreq
 
-
-#To access each individual file frequency
-print(anger_f0[1].shape)
+# Read MFCCs
+anger_mfcc = np.load('mfcc_anger.npy')
+neut_mfcc = np.load('mfcc_neutral.npy')
