d57105630236168b20ba6b091cc1d9ef4aa64620
diepjess <jdiep@C02Y50F2JHD2.local>
2019-04-20 20:14:27 -0400

    jesswork

--- read_features.py
+++ read_features.py
@@ -3,10 +3,15 @@ import numpy as np
 # Read Fundamental Frequency
 anger_f0 = np.load('fundfreq_anger.npy')
 neut_f0 = np.load('fundfreq_neutral.npy')
+anger_mfcc = np.load('mfcc_anger.npy')
+neut_mfcc = np.load('mfcc_neutral.npy')
 
 print(anger_f0.shape)
 print(neut_f0.shape)
-
+print(anger_mfcc.shape)
+print(neut_mfcc.shape)
 
 #To access each individual file frequency
 print(anger_f0[1].shape)
+print(anger_f0[1])
+print(anger_f0[2])
